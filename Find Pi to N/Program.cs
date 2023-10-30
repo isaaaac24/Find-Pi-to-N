e3571d6088044af823446c125ce8129419681fff
isaaaac24 <zacwrightson@gmail.com>
2023-10-30 15:21:54 +0000

    Added basic file reading to facilitate reading n digits of pi.txt

--- Find Pi to N/Program.cs
+++ Find Pi to N/Program.cs
@@ -1,8 +1,32 @@
-﻿// Main program class
+﻿using System.IO;
+using System.Text;
+
+// Main program class
 class Program
 {
     static void Main(string[] args)
     {
-        
+        Console.WriteLine("Enter the number of digits to calculate pi to: ");
+        int requiredDigits = int.Parse(Console.ReadLine());
+
+        int digitCount = 0;
+        string filePath = "Resources/pi.txt";
+        StringBuilder digits = new StringBuilder();
+
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            int currentChar;
+            while ((currentChar = reader.Read()) != -1 && digitCount < requiredDigits)
+            {
+                if (Char.IsDigit((char)currentChar))
+                {
+                    digits.Append((char)currentChar);
+                    digitCount++;
+                }
+            }
+
+        }
+
+        Console.WriteLine(digits.ToString());
     }
 }
