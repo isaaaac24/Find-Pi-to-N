862aea05d9557bffa8d59271573044e8eed04a98
isaaaac24 <zacwrightson@gmail.com>
2023-10-30 16:10:34 +0000

    Commented code

--- Find Pi to N/Program.cs
+++ Find Pi to N/Program.cs
@@ -1,13 +1,16 @@
-﻿using System.IO;
+﻿// Imports
+using System.IO;
 using System.Text;
 
 // Main program class
 class Program
 {
+    //Delcaring constants
     const string filePath = "Resources/pi.txt";
     const int maxDigits = 1000;
     const int minDigits = 1;
 
+    //Main program execution
     static void Main(string[] args)
     {
         int requiredDigits = GetDigitCount();
@@ -15,13 +18,17 @@ class Program
         Console.WriteLine($"3.{piDigits.ToString()}");
     }
 
+    // Takes user input to dictate number of decimal values of pi to print
     static int GetDigitCount()
     {
+        // Loops input request until acceptable value is produced
         while (true)
         {
             Console.WriteLine("Enter the number of digits to calculate pi to: ");
+            // Validates whether input is integer
             if (int.TryParse(Console.ReadLine(), out int requiredDigits))
             {
+                // Validates whether integer value is between specified min and max digits
                 if (requiredDigits <= maxDigits && requiredDigits >= minDigits)
                 {
                     return requiredDigits;
@@ -38,16 +45,19 @@ class Program
         }
     }
 
+    // Reads pi.txt to the specified amount of digits
     static string ReadPiDigits(int requiredDigits)
     {
         int digitCount = 0;
         StringBuilder digits = new StringBuilder();
 
+        // Attempts to read pi.txt
         try
         {
             using (StreamReader reader = new StreamReader(filePath))
             {
                 int currentChar;
+                // Reads each character from pi.txt until there are no more characters to read, or the amount of characters read has reached the amount required
                 while ((currentChar = reader.Read()) != -1 && digitCount < requiredDigits)
                 {
                     if (Char.IsDigit((char)currentChar))
@@ -58,14 +68,17 @@ class Program
                 }
             }
         }
+        // Unable to find file
         catch (FileNotFoundException)
         {
             Console.WriteLine("The file could not be found.");
         }
+        // I/O exception occurs
         catch (IOException)
         {
             Console.WriteLine("An I/O error occurred while reading the file");
         }
+        // UNexpected exceptions
         catch (Exception ex)
         {
             Console.WriteLine($"An unexpected error occurred: {ex.Message}");
