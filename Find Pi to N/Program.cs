b16c8370fe6a7f1c7651879f8652eeb621271429
isaaaac24 <zacwrightson@gmail.com>
2023-10-30 15:43:32 +0000

    Added max value of digits for pi with error handling

--- Find Pi to N/Program.cs
+++ Find Pi to N/Program.cs
@@ -6,19 +6,30 @@ using System.Text;
 class Program
 {
     const string filePath = "Resources/pi.txt";
+    const int maxDigits = 1000;
+
     static void Main(string[] args)
     {
         int requiredDigits = GetDigitCount();
         string piDigits = ReadPiDigits(requiredDigits);
-
         Console.WriteLine("3." + piDigits.ToString());
     }
 
     static int GetDigitCount()
     {
-        Console.WriteLine("Enter the number of digits to calculate pi to: ");
-        int requiredDigits = int.Parse(Console.ReadLine());
-        return requiredDigits;
+        while (true)
+        {
+            Console.WriteLine("Enter the number of digits to calculate pi to: ");
+            int requiredDigits = int.Parse(Console.ReadLine());
+            if (requiredDigits <= maxDigits)
+            {
+                return requiredDigits;
+            }  
+            else
+            {
+                Console.WriteLine("Please input a number lower than 1000");
+            }
+        }
     }
 
     static string ReadPiDigits(int requiredDigits)
